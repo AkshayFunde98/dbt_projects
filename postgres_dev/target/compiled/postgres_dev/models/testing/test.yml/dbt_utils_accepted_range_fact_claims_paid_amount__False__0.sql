3bba5b0f049c65a0da344a65a47636c58876939b
AkshayFunde98 <akshayfunde98@gmail.com>
2024-03-27 17:32:58 +0530

    update test file

---
+++ postgres_dev/target/compiled/postgres_dev/models/testing/test.yml/dbt_utils_accepted_range_fact_claims_paid_amount__False__0.sql
@@ -0,0 +1,20 @@
+
+
+with meet_condition as(
+  select *
+  from "bootcamp_project"."public_silver"."fact_claims"
+),
+
+validation_errors as (
+  select *
+  from meet_condition
+  where
+    -- never true, defaults to an empty result set. Exists to ensure any combo of the `or` clauses below succeeds
+    1 = 2
+    -- records with a value >= min_value are permitted. The `not` flips this to find records that don't meet the rule.
+    or not paid_amount > 0
+)
+
+select *
+from validation_errors
+
